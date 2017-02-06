1a96f5453b29449281a3c6929796ffc5f9a4c83d
nabuckein <pauri@hotmail.com>
2017-02-06 15:22:31 -0600

    Add files via upload

--- Owner.js
+++ Owner.js
@@ -10,60 +10,56 @@ var userInfo = {
 	lastName : 'empty',
 	email: 'empty'
 };
+
 var arrayOfReviewsFirstName = [];
 var arrayOfReviewsLastName = [];
 var arrayOfReviewsEmail = [];
 var arrayOfReviews = [];
+var rows = [];
 
 var firebaseRef = firebase.database().ref('users/');
-firebaseRef.on('value', function(snapshot) {
-  snapshot.forEach(function(childSnapshot) {
-        var childKey = childSnapshot.key;
-        var childData = childSnapshot.val();
-        userInfo.firstName = childKey;
-        arrayOfReviewsFirstName.push(childData.firstName);
-        arrayOfReviewsLastName.push(childData.lastName);
-        arrayOfReviewsEmail.push(childData.email);
-        arrayOfReviews.push(childData.review);
-      });  
-});
+	    firebaseRef.on('value', function(snapshot) {
+	  	snapshot.forEach(function(childSnapshot) {
+	        var childKey = childSnapshot.key;
+	        var childData = childSnapshot.val();
+	        userInfo.firstName = childKey;
+	        arrayOfReviewsFirstName.push(childData.firstName);
+	        arrayOfReviewsLastName.push(childData.lastName);
+	        arrayOfReviewsEmail.push(childData.email);
+	        arrayOfReviews.push(childData.review);
+	      //});  
+	    	});
+	  	});
 
 class Owner extends Component {
 
-  state = {
-    refreshReviewsList: false,
-    reviews: arrayOfReviews[0],
-    firstNames : arrayOfReviewsFirstName,
-    lastNames : arrayOfReviewsLastName,
-    emails: arrayOfReviewsEmail
-  }
+	componentWillUpdate = (e) =>{
+		arrayOfReviewsFirstName = [];
+		arrayOfReviewsLastName = [];
+		arrayOfReviewsEmail = [];
+		arrayOfReviews = [];
+		rows = [];
 
-  handleApproveClick = (e) =>{
-    arrayOfReviews.pop();
-    console.log(arrayOfReviews);
-    this.setState({reviews:arrayOfReviews[1]});
-  }
+		
+	}
 
   render() {
-  	
+  		rows = [];
+		for (var i=0; i <= arrayOfReviews.length-1; i++) {			
+		    rows.push(<OwnerReviewApproval userFirstName={arrayOfReviewsFirstName[i]} userLastName={arrayOfReviewsLastName[i]} userEmail={arrayOfReviewsEmail[i]} userReview={arrayOfReviews[i]}/>
+			);
+		}
 	    return (
 	      <div>
 	        <div className="CompTitle">
 	          <p id="componentTitle">Pending Reviews</p> 
 	        </div>
 	        <div className="reviewsContainer text-center" id="approveAndRemoveButtons">	        	
-            <OwnerReviewApproval approveHandler={this.handleApproveClick} firstName={this.state.reviews} lastName={arrayOfReviewsLastName[0]} review={arrayOfReviews[0]}/>
-            <OwnerReviewApproval firstName={arrayOfReviewsFirstName[1]} lastName={arrayOfReviewsLastName[1]} review={arrayOfReviews[1]}/>
-	        </div>
+ 				{rows}
+ 			</div>
 	      </div>
-	    );
-
-
-	
+	    );	
   }
-
 }
-
-
 
 export default Owner;
